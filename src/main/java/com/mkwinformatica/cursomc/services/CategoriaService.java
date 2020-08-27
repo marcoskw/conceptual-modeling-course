8b32044ca468c07eda29fc68f2cdfa14a548ffe4
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2020-08-27 16:28:11 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/mkwinformatica/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.mkwinformatica.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mkwinformatica.cursomc.domain.Categoria;
+import com.mkwinformatica.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;	
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
