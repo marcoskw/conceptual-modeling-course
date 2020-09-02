b11644fe2587dce0b3f648ee706c4aef063a860a
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2020-09-02 14:56:13 -0300

    Cliente, TipoCliente, Telefones e Endereços

--- src/main/java/com/mkwinformatica/cursomc/CursomcApplication.java
+++ src/main/java/com/mkwinformatica/cursomc/CursomcApplication.java
@@ -67,6 +67,5 @@ public class CursomcApplication implements CommandLineRunner{
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
 	}
 }
