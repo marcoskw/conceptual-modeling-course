11c3e9d1ab620bc8dc4e637d68a31efdcb40757a
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2020-09-10 09:47:16 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/mkwinformatica/cursomc/services/CategoriaService.java
+++ src/main/java/com/mkwinformatica/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mkwinformatica.cursomc.domain.Categoria;
+import com.mkwinformatica.cursomc.dto.CategoriaDTO;
 import com.mkwinformatica.cursomc.repositories.CategoriaRepository;
 import com.mkwinformatica.cursomc.services.exceptions.DataIntegrityException;
 import com.mkwinformatica.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
