9f66e352ece383d1de40686029c61f05bda036f7
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2020-11-06 14:57:54 -0300

    Implementando toString de Pedido

--- src/main/java/com/mkwinformatica/cursomc/domain/ItemPedido.java
+++ src/main/java/com/mkwinformatica/cursomc/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.mkwinformatica.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -8,19 +10,19 @@ import javax.persistence.Entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class ItemPedido implements Serializable{
+public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	
+
 	public ItemPedido() {
-		
+
 	}
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
@@ -31,24 +33,24 @@ public class ItemPedido implements Serializable{
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
-	
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
 	public void setPedido(Pedido pedido) {
 		id.setPedido(pedido);
 	}
-	
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-	
+
 	public void setProduto(Produto produto) {
 		id.setProduto(produto);
 	}
@@ -109,5 +111,19 @@ public class ItemPedido implements Serializable{
 			return false;
 		return true;
 	}
-	
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+		builder.append(", Qtde: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubTotal()));
+		builder.append("\n");
+		return builder.toString();
+	}
 }
--- src/main/java/com/mkwinformatica/cursomc/services/PedidoService.java
+++ src/main/java/com/mkwinformatica/cursomc/services/PedidoService.java
@@ -15,12 +15,11 @@ import com.mkwinformatica.cursomc.repositories.ItemPedidoRepository;
 import com.mkwinformatica.cursomc.repositories.PagamentoRepository;
 import com.mkwinformatica.cursomc.repositories.PedidoRepository;
 import com.mkwinformatica.cursomc.services.exceptions.ObjectNotFoundException;
-
 @Service
 public class PedidoService {
-
+	
 	@Autowired
-	private PedidoRepository repository;
+	private PedidoRepository repo;
 	
 	@Autowired
 	private BoletoService boletoService;
@@ -29,36 +28,41 @@ public class PedidoService {
 	private PagamentoRepository pagamentoRepository;
 	
 	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
 	private ProdutoService produtoService;
 	
 	@Autowired
-	private ItemPedidoRepository itemPedidoRepository;
-
+	private ClienteService clienteService;
+	
 	public Pedido find(Integer id) {
-		Optional<Pedido> obj = repository.findById(id);
+		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
-
+	
 	@Transactional
 	public Pedido insert(Pedido obj) {
-
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
-		obj = repository.save(obj);
+		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
