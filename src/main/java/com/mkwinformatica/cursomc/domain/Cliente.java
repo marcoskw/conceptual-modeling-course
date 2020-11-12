5d2f35722617350ab8d45029e4d29f78a268a6dd
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2020-11-12 14:13:15 -0300

    Salvando perfis de usuário no banco de dados

--- src/main/java/com/mkwinformatica/cursomc/domain/Cliente.java
+++ src/main/java/com/mkwinformatica/cursomc/domain/Cliente.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mkwinformatica.cursomc.domain.enums.Perfil;
 import com.mkwinformatica.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -45,12 +48,16 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
+	
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
-		
+		addPeril(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
@@ -61,6 +68,7 @@ public class Cliente implements Serializable{
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo == null) ? null : tipo.getCodigo();
 		this.senha = senha;
+		addPeril(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
@@ -127,7 +135,6 @@ public class Cliente implements Serializable{
 		this.pedidos = pedidos;
 	}
 	
-
 	public String getSenha() {
 		return senha;
 	}
@@ -135,6 +142,14 @@ public class Cliente implements Serializable{
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}	
+	
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	
+	public void addPeril(Perfil perfil) {
+		perfis.add(perfil.getCodigo());
+	}
 
 	@Override
 	public int hashCode() {
--- src/main/java/com/mkwinformatica/cursomc/services/DBService.java
+++ src/main/java/com/mkwinformatica/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.mkwinformatica.cursomc.domain.PagamentoComCartao;
 import com.mkwinformatica.cursomc.domain.Pedido;
 import com.mkwinformatica.cursomc.domain.Produto;
 import com.mkwinformatica.cursomc.domain.enums.EstadoPagamento;
+import com.mkwinformatica.cursomc.domain.enums.Perfil;
 import com.mkwinformatica.cursomc.domain.enums.TipoCliente;
 import com.mkwinformatica.cursomc.repositories.CategoriaRepository;
 import com.mkwinformatica.cursomc.repositories.CidadeRepository;
@@ -98,9 +99,13 @@ public class DBService {
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "marcoskorbeswerle@hotmail.com", "3678912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		Cliente cli2 = new Cliente(null, "Ana Costa", "marcoskwerle@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		
+		cli2.addPeril(Perfil.ADMIN);
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2004", null, "Centro", "74266424", cli2, c2);
 				
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
@@ -136,8 +141,10 @@ public class DBService {
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		cli2.getTelefones().addAll(Arrays.asList("35742468", "35784257"));
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
 		ped1.setPagamento(pagto1);
 		ped2.setPagamento(pagto2);
@@ -155,8 +162,8 @@ public class DBService {
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
